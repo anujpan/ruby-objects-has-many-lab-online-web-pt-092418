789e98867c2099ad4e78a99afc79b98dbfdb351f
Anuj Panchal <anujpan1@gmail.com>
2018-10-30 00:09:23 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,19 +2,18 @@ class Author
   attr_accessor :name, :posts
    @@post_count = 0
    def initialize(name)
-    @name = name #new is initialized with a name
-    #name has an attr_accessor for name
+    @name = name
     @posts = []
   end
    def add_post(post)
     @posts << post
-    post.author = self #uses the class as a method. takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist
+    post.author = self
     @@post_count += 1
   end
    def add_post_by_title(title)
-    post = Post.new(title) #creates a new post with a author
+    post = Post.new(title)
     @posts.push(post)
-    post.author = self #associates the song and artist
+    post.author = self
     @@post_count += 1
   end
    def self.post_count
